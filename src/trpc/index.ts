4ead6b48f28a2975b78db6d7fead831bc40979ba
Anantha Krishnan KS <ananthakrishnan.s@scrapehero.com>
2023-12-10 13:06:42 +0530

    created an authCallback api to check auth

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,19 @@
 import { publicProcedure, router } from "./trpc";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError } from "@trpc/server";
+
 export const appRouter = router({
-	
+	authCallback: publicProcedure.query(async () => {
+		const { getUser } = getKindeServerSession();
+		const user = await getUser();
+
+		//Throw an unauthorized error if the user is not authenticated
+		if (!user || !user.id) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+		//Check if the user is in the database
+
+		return { success: true };
+	}),
 });
 // Export type router type signature,
 // NOT the router itself.
